e03aa5bb223c7c2b7d9e2c7004cf1f16491a6f37
ERJLM <99908664+ERJLM@users.noreply.github.com>
2023-05-15 16:19:53 +0100

    Update Node.java

--- Decision Tree/Node.java
+++ Decision Tree/Node.java
@@ -1,26 +1,79 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Node {
+    Node parent;
     String label;
-   
+    String branch;
     List<Node> children;
+    Map<String, Map<String,String>> examples = new HashMap<>();
+
+    Node(String l, String b, Node p){
+        label = l;
+        children = new ArrayList<>();
+        parent = p;
+        branch = b;
+       }
+       Node(List<Node> c, String l){
+        label = l;
+        children = c;
+        parent = null;
+        branch = "";
+       }
+
     Node(String l){
      label = l;
      children = new ArrayList<>();
-     
+     parent = null;
+     branch = "";
     }
     Node(){
         label = "";
         children = new ArrayList<>();
-        
+        parent = null;
+        branch = "";
        }
     
     String getLabel(){
         return label;
     }
+    String getBranch(){
+        return branch;
+    }
+
+    List<Node> getChildren(){
+        return children;
+    }
+
+    Map<String, Map<String,String>> getExamples(){
+        return examples;
+    }
+    Node getParent(){
+        return parent;
+    }
+    
 
     void setLabel(String l){
         label = l;
     }
+
+    void setExamples(Map<String, Map<String,String>> ex){
+        examples = ex;
+    }
+
+    void setChildren(List<Node> c){
+        children = c;
+    }
+
+    void setBranch(String b){
+        branch = b;
+    }
+
+    void setParent(Node p){
+        parent = p;
+    }
+
+    
 }
